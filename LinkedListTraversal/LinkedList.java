3c07f5e86bce0ceb040f1bd0042b57b064df84ef
Prabha Merlin Alex <prabhamerlin@gmail.com>
2018-12-07 12:19:09 +0530

    Create LinkedList.java

---
+++ LinkedListTraversal/LinkedList.java
@@ -0,0 +1,44 @@
+package dataStructures;
+
+public class LinkedList 
+{
+	Node head;
+	Node n;
+	
+	Node temp = head;
+	public LinkedList() 
+	{
+		
+	}
+	public void display(Node head)
+	{
+		Node n = head;
+		do
+		{
+			System.out.println(n.num);
+			n = n.next;
+		}while(n!=null);
+	}
+	public void addElements(Node n) 
+	{
+		if(head == null)
+		{
+			head = n;
+			temp = n;
+			//head.next = null;
+		}
+		
+		else
+		{
+			if(n!=null)
+			{
+				temp.next = n;	
+			}
+			temp = n;
+		}
+		
+		
+	}
+
+	
+}
